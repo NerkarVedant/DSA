5e272b4d4c58ed8d8c14886b1051eee038dc4698
Vedant.N <nerkarvedant25@gmail.com>
2026-02-15 23:15:56 +0530

    16 Feb 2026 reverse Number

---
+++ java_Dsa/reverseNumber.java
@@ -0,0 +1,37 @@
+static class Solution {
+    public int reverse(int x) {
+
+        int rev =0;
+
+        while (x != 0){
+
+            int digit = x % 10;
+
+            if (rev > Integer.MAX_VALUE / 10 ||
+                    (rev == Integer.MAX_VALUE / 10 && digit > 7))
+                return 0;
+
+            if (rev < Integer.MIN_VALUE / 10 ||
+                    (rev == Integer.MIN_VALUE / 10 && digit < -8))
+                return 0;
+
+
+            rev = rev * 10 + digit;
+
+            x = x/10;
+
+        }
+
+        return rev;
+
+
+
+    }
+}
+
+void main() {
+    Solution sol = new Solution();
+    int input = -1234;
+    int result = sol.reverse(input);
+    System.out.println("Reverse of " + input + " is: " + result);
+}
